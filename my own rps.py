121dd0ab1cb6535820ee56e4b561f2168ba6927a
Entility <83473910+Entility@users.noreply.github.com>
2022-09-21 09:12:11 +0300

    Add files via upload

---
+++ my own rps.py
@@ -0,0 +1,32 @@
+import random
+human_turn=input(print('whawill you pick: ','Rock? Scissors? Paper?'))
+print('Human chose: ',human_turn,'.')
+
+choices=('Rock','Scissors','Paper')
+
+computer_turn=random.randint(0,2)
+computer_turn=choices[computer_turn]
+print('Computer chose: ',computer_turn,'.')
+
+if human_turn == 'Rock' and computer_turn =='Scissors':
+    print('Human wins!')
+elif computer_turn == 'Rock' and human_turn =='Scissors':
+    print('Computer wins!')
+
+    
+elif human_turn == 'Paper' and computer_turn =='Rock':
+    print('Human wins!')
+elif computer_turn == 'Paper' and human_turn =='Rock':
+    print('Computer wins!')
+
+elif human_turn == 'Scissors' and computer_turn =='Paper':
+    print('Human wins!')
+elif computer_turn == 'Scissors' and human_turn =='Paper':
+    print('Computer wins!')
+
+else:
+    print('Draw')
+
+
+
+
